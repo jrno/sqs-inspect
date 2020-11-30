0b80984f9298275704ccc57763d90893874812e2
Jarno Pohjonen <jarno.pohjonen@gmail.com>
2020-11-30 22:57:35 +0200

    Add new arguments and tweak the process

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-Simple utility to inspect a sqs queue in aws for data
+# sqs-inspect
 
-- Yargs for argument parsing
-- Aws sdk for connecting to aws
+Simple tool to download and inspect queue data from SQS. Useful for example when inspecting items from dead letter queue. Sqs doesn't provide any good interface to inspect the messages for certain data.
+
+## How it works
+
+Simply receives all messages from given queue and produce the final result to a json file. Messages are fetched with all attributes.
+
+## How to run
+
+TODO: fill
+
+## TODO:
+
+- build/run without ts-node
+- add --search argument for inspection
--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,19 @@ import yargs, { Argv } from 'yargs'
 import * as fs from 'fs'
 import { Message } from 'aws-sdk/clients/sqs'
 
-const visibilityTimeout = 60 
-const messagesPerReceive = 1 // 1-10
-
 async function getMessageCount(sqs: SQS, queueUrl: string): Promise<number> {
     return new Promise((resolve, reject) => {
         sqs.getQueueAttributes({QueueUrl: queueUrl, AttributeNames: ["All"]}, (err, data) => {
-            console.log
             if (err) {
                 reject(err)
-            } else if (data.Attributes) {
-                console.log(data.Attributes)
-                resolve(Number(data.Attributes.ApproximateNumberOfMessages))
+            } else {
+                resolve(Number(data?.Attributes?.ApproximateNumberOfMessages ?? 0))
             }
         })
     })
 }
 
-async function receiveMessages(sqs: SQS, queueUrl: string): Promise<Message[]> {
+async function receiveMessages(sqs: SQS, queueUrl: string, messagesPerReceive: number = 10, visibilityTimeout: number = 30): Promise<Message[]> {
     return new Promise((resolve, reject) => {
         sqs.receiveMessage({
             MaxNumberOfMessages: messagesPerReceive,
@@ -31,8 +26,8 @@ async function receiveMessages(sqs: SQS, queueUrl: string): Promise<Message[]> {
         }, (err, data) => {
             if (err) {
                 reject(err)
-            } else if (data.Messages) {
-                resolve(data.Messages)
+            } else {
+                resolve(data?.Messages ?? [])
             }
         })
     })
@@ -41,62 +36,87 @@ async function receiveMessages(sqs: SQS, queueUrl: string): Promise<Message[]> {
 function getArgs(): Argv {
     return yargs(process.argv)
         .option('aws_region', {
+            describe: "Aws region",
+            demand: false,
             default: 'eu-north-1'
         })
         .option('aws_access_key', {
+            describe: "Aws access key",
             demand: true
         })
         .option('aws_secret_key', {
+            describe: "Aws secret access key",
             demand: true,
         })
-        .option('queue_url', {
-            describe: 'Sqs queue url',
+        .option('aws_session_token', {
+            describe: "Optional session token",
+            demand: false,
+        })
+        .option('sqs_queue_url', {
+            describe: 'Sqs queue endpoint url',
             demand: true
         })
-        .option('filename', {
-            describe: 'Filename to write the output, defauts to sqs-inspect.json',
+        .option('max_messages_per_receive', {
+            describe: 'Max number of messages per receive. 1 - 10',
+            default: 10
+        })
+        .option('visibility_timeout', {
+            describe: 'Time in seconds that received messages are hidden in queue. Adjust to a value that is larger than the process time.',
+            default: 30
+        })
+        .option('outfile', {
+            describe: 'Output filename',
             demand: false,
+            default: 'sqs-inspect.json'
         })
         .help('h')
 }
 
 async function run() {
-    const argv = getArgs().argv
+
+    const args = getArgs().argv
+    console.log('Sqs inspect started. Process arguments:', args)
+
     const sqs = new SQS({
-        region: argv.aws_region as string,
+        region: args.aws_region as string,
         credentials: {
-            accessKeyId: argv.aws_access_key as string,
-            secretAccessKey: argv.aws_secret_key as string
+            accessKeyId: args.aws_access_key as string,
+            secretAccessKey: args.aws_secret_key as string,
+            sessionToken: args.aws_session_token ? args.aws_session_token as string : undefined 
         }
     })
-    const fileName = argv.filename ? argv.filename as string : `sqs-inspect.json` as string
-    const queueUrl = argv.queue_url as string
-    console.log(`Fetching from ${queueUrl}. max_messages_per_poll: ${messagesPerReceive}, visibility_timeout: ${visibilityTimeout}`)
-    console.log("Fetching approximate count of messages...")
-    let queuedMessages = await getMessageCount(sqs, queueUrl);
+    const outFile = args.outfile as string
+    const sqsQueueUrl = args.sqs_queue_url as string
+    const maxMessagesPerReceive = args.max_messages_per_receive as number
+    const visibilityTimeout = args.visibility_timeout as number
+
+    console.log("Fetch approximate count of available messages")
+    let queuedMessages = await getMessageCount(sqs, sqsQueueUrl);
 
     if (queuedMessages === 0) {
-        console.log("Queue doesn't have any (visible) messages. Process will exit...")
+        console.log("Queue doesn't have any available messages, process will exit")
         process.exit()
     }
 
     let messages: Message[] = []
     while (messages.length < queuedMessages) {
-        messages = messages.concat(await receiveMessages(sqs, queueUrl))
-        console.log(`${messages.length} of ${queuedMessages} messages received...`)
+        process.stdout.write(`Polling new messages... `)
+        messages = messages.concat(await receiveMessages(sqs, sqsQueueUrl, maxMessagesPerReceive, visibilityTimeout))
+        console.log(`${messages.length} of ${queuedMessages} fetched`)
     }
 
-    fs.writeFile(fileName, JSON.stringify(messages, null, 2), () => {
-        console.log(`Output written to ${fileName}`)
+    console.log(`Writing results to file`)
+    fs.writeFile(outFile, JSON.stringify(messages, null, 2), () => {
+        console.log(`Results stored to ${outFile}`)
     });
 }
 
-run()
+try {
+    run()
+} catch (err) {
+    console.error("Unexpected error occured", err)
+}
 
-// TODO: region as input
-// TODO: receive messages from queue
-// TODO: write received messages with all attributes to local folder
 // TODO: check how to grep data from the folder
-// TODO: omit download if data exists. --tmp_path = "./ted-local"
 // TODO: omit search if phrase not given --search "123456"
 
